830a37025818110d7af479654f2c6c99255cc5e6
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-07-25 17:24:14 -0400

    random button works

--- index.js
+++ index.js
@@ -2,24 +2,46 @@
 
 const URL1 = "https://animechan.vercel.app/api/random"
 
-const randomQuote = document.querySelector('#random');
-
-randomQuote.addEventListener('click', getQuote);
+const randomBtnTag = document.querySelector('.container random');
+const animeQuote = document.querySelector('#anime-quote');
+const animeName = document.querySelector('#anime-name');
+const characterName = document.querySelector('#character-name');
 
 function getQuote(){
-    return fetch(URL1, {
+    fetch(URL1, {
         headers: {
             'Accept': 'application/json'
         }
     })
     .then(response => response.json())
-    .then(quote => console.log(quote))
+    .then(quote => {
+        animeQuote.innerText = quote.quote;
+        animeName.innerText = quote.anime;
+        characterName.innerText= quote.character;
+        })
 }
 
-getQuote();
-genRandomBtn.addEventListener('click', ()=>{
+
+
+random.addEventListener('click', ()=>{
     getQuote();
 })
 
+const characterText = document.querySelector('#text');
+const submitBtn = document.querySelector('#submit');
+const resultsTag = document.querySelector('#results');
+const userInput = document.getElementById("text");
+
+submitBtn.addEventListener('click', getQuoteByName(userInput));
+
+function getQuoteByName(nameVariable){
+    console.log('name', nameVariable)
+    fetch(`https://animechan.vercel.app/api/quotes/character?name=${nameVariable}`)
+      .then(response => response.json())
+      .then(quotes => {
+          console.log(quotes)
+          //resultsTag.innerText = quotes.anime;
+      })
+}
 
 
